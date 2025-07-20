65ada528f20f1487dab864b7a7773e66c382f210
Peter Schwake <peter.schwake@gmail.com>
2025-07-20 19:22:39 +0200

    Update slow_load.lua

--- Custom Avionics/animation/slow_load.lua
+++ Custom Avionics/animation/slow_load.lua
@@ -9,28 +9,28 @@ end
 
 -- Register DataRefs
 defineProps({
-    {"frame_time",        "tu154ce/time/frame_time",                 globalPropertyf},
-    {"tank_1_pr",         "tu154ce/payload/tank_1",                  globalPropertyi},
-    {"tank_4_pr",         "tu154ce/payload/tank_4",                  globalPropertyi},
-    {"tank_2L_pr",        "tu154ce/payload/tank_2L",                 globalPropertyi},
-    {"tank_2R_pr",        "tu154ce/payload/tank_2R",                 globalPropertyi},
-    {"tank_3L_pr",        "tu154ce/payload/tank_3L",                 globalPropertyi},
-    {"tank_3R_pr",        "tu154ce/payload/tank_3R",                 globalPropertyi},
-    {"fuel_tanker_call",  "tu154ce/anim/fuel_tanker_call",           globalPropertyi},
-    {"gear_blocks",       "tu154ce/anim/gear_blocks",                globalPropertyi},
-    {"fuel_tanker",       "tu154ce/anim/fuel_tanker",                globalPropertyf},
-    {"slider_5",          "sim/cockpit2/switches/custom_slider_on[4]",  globalPropertyi},
-    {"payload",           "sim/flightmodel/weight/m_fixed",             globalPropertyf},
-    {"CG_load",           "sim/flightmodel/misc/cgz_ref_to_default",    globalPropertyf},
-    {"fuel_q_1",          "sim/flightmodel/weight/m_fuel[0]",           globalPropertyf},
-    {"fuel_q_4",          "sim/flightmodel/weight/m_fuel[1]",           globalPropertyf},
-    {"fuel_q_2R",         "sim/flightmodel/weight/m_fuel[2]",           globalPropertyf},
-    {"fuel_q_2L",         "sim/flightmodel/weight/m_fuel[3]",           globalPropertyf},
-    {"fuel_q_3R",         "sim/flightmodel/weight/m_fuel[4]",           globalPropertyf},
-    {"fuel_q_3L",         "sim/flightmodel/weight/m_fuel[5]",           globalPropertyf},
-    {"paylod_set",        "tu154ce/payload/paylod_set",              globalPropertyf},
-    {"cg_set",            "tu154ce/payload/cg_set",                  globalPropertyf},
-    {"load_slow_btn",     "tu154ce/payload/load_slow_btn",           globalPropertyi},
+    {"frame_time", 			"tu154ce/time/frame_time",                 			globalPropertyf},
+    {"tank_1_pr", 				"tu154ce/payload/tank_1",                  		globalPropertyi},
+    {"tank_4_pr", 			"tu154ce/payload/tank_4",                 			globalPropertyi},
+    {"tank_2L_pr", 			"tu154ce/payload/tank_2L",                 			globalPropertyi},
+    {"tank_2R_pr",       	"tu154ce/payload/tank_2R",                			globalPropertyi},
+    {"tank_3L_pr", 			"tu154ce/payload/tank_3L",                 			globalPropertyi},
+    {"tank_3R_pr",       	"tu154ce/payload/tank_3R",                 			globalPropertyi},
+    {"fuel_tanker_call",  	"tu154ce/anim/fuel_tanker_call",           			globalPropertyi},
+    {"gear_blocks",       	"tu154ce/anim/gear_blocks",                			globalPropertyi},
+    {"fuel_tanker",       	"tu154ce/anim/fuel_tanker",                			globalPropertyf},
+    {"slider_5",          		"sim/cockpit2/switches/custom_slider_on[4]",	globalPropertyi},
+    {"payload",           		"sim/flightmodel/weight/m_fixed",             	globalPropertyf},
+    {"CG_load",           		"sim/flightmodel/misc/cgz_ref_to_default",		globalPropertyf},
+    {"fuel_q_1",          		"sim/flightmodel/weight/m_fuel[0]",           	globalPropertyf},
+    {"fuel_q_4",          		"sim/flightmodel/weight/m_fuel[1]",           	globalPropertyf},
+    {"fuel_q_2R",         	"sim/flightmodel/weight/m_fuel[2]",           		globalPropertyf},
+    {"fuel_q_2L",         		"sim/flightmodel/weight/m_fuel[3]",           	globalPropertyf},
+    {"fuel_q_3R",         	"sim/flightmodel/weight/m_fuel[4]",           		globalPropertyf},
+    {"fuel_q_3L",         		"sim/flightmodel/weight/m_fuel[5]",           	globalPropertyf},
+    {"paylod_set",        		"tu154ce/payload/paylod_set",              		globalPropertyf},
+    {"cg_set",            		"tu154ce/payload/cg_set",                  		globalPropertyf},
+    {"load_slow_btn",     	"tu154ce/payload/load_slow_btn",           			globalPropertyi},
 })
 
 -- Convert boolean to integer
@@ -99,27 +99,22 @@ function update()
                     if t2R2 then set(fuel_q_2R, math.min(get(fuel_q_2R) + dt * 64 / cnt, 3700)) end
                 end
             end
-            -- Tanks 2 stage 3
-            do
-                local t2L3 = get(tank_2L_pr) > get(fuel_q_2L) and get(fuel_q_2L) < 9500
-                           and not (get(tank_2L_pr) > get(fuel_q_2L) < 3700)
-                           and not t1
-                local t2R3 = get(tank_2R_pr) > get(fuel_q_2R) and get(fuel_q_2R) < 9500
-                           and not (get(tank_2R_pr) > get(fuel_q_2R) < 3700)
-                           and not t1
-                local cnt = bool2int(t2L3) + bool2int(t2R3)
-                if cnt > 0 then
-                    if t2L3 then set(fuel_q_2L, math.min(get(fuel_q_2L) + dt * 64 / cnt, 9500)) end
-                    if t2R3 then set(fuel_q_2R, math.min(get(fuel_q_2R) + dt * 64 / cnt, 9500)) end
-                end
-            end
+			-- Tanks 2 stage 3
+			do
+				local t2L3 = get(tank_2L_pr) > get(fuel_q_2L) and get(fuel_q_2L) < 9500 and not t2L2 and not t1
+				local t2R3 = get(tank_2R_pr) > get(fuel_q_2R) and get(fuel_q_2R) < 9500 and not t2R2 and not t1
+				local cnt = bool2int(t2L3) + bool2int(t2R3)
+				if cnt > 0 then
+					if t2L3 then set(fuel_q_2L, math.min(get(fuel_q_2L) + dt * 64 / cnt, 9500)) end
+					if t2R3 then set(fuel_q_2R, math.min(get(fuel_q_2R) + dt * 64 / cnt, 9500)) end
+				end
+			end
             -- Tank 4
             local t4 = get(tank_4_pr) > get(fuel_q_4) and get(fuel_q_4) < 6598
             if t4 then
                 set(fuel_q_4, math.min(get(fuel_q_4) + dt * 64, 6598))
             end
         end
-
         -- Calculate total load duration
         local load_time = 600 * get(paylod_set) / 20000
         if load_time == 0 and get(fuel_tanker_call) == 0 and fuel_load then
@@ -128,21 +123,17 @@ function update()
             -- Animate slider
             set(slider_5, 1)
             load_timer = load_timer + dt
-
             -- Increment payload
             set(payload, math.min(get(payload) + dt * 33.333333, get(paylod_set)))
-
             -- Capture CG targets once
             if not cg_read then
                 cg_read = true
                 CG_old  = get(CG_load)
                 CG_need = get(cg_set)
             end
-
             -- Smoothly adjust CG
             local CG_spd = load_time > 0 and ((CG_need - CG_old) / load_time) or 0
             set(CG_load, get(CG_load) + dt * CG_spd)
-
             -- Finish loading
             if load_timer >= load_time then
                 set(CG_load, CG_need)
